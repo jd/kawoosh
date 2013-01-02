ab00a36723b3235156578bf7addf31353350c2ab
Julien Danjou <julien@danjou.info>
2013-01-02 18:53:05 +0100

    Remove clj
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- src/kawoosh/models/db.clj
+++
@@ -1,7 +0,0 @@
-(ns kawoosh.models.db)
-
-;; TODO use a configuration file
-(def db {:subprotocol "postgresql"
-         :subname "//localhost/kawoosh"
-         :user "kawoosh"
-         :password "kawoosh"})
