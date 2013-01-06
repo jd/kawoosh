17bb684d64b5a6034f97070cbb5866ffe3f92ec8
Julien Danjou <julien@danjou.info>
2013-01-06 23:04:44 +0100

    Proto user-get
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- httpd.lisp
+++ httpd.lisp
@@ -2,19 +2,31 @@
   (:use cl
         kawoosh-dao
         clack
-        clack.app.route))
+        clack.app.route)
+  (:export :start))
 
 (in-package :kawoosh-httpd)
+
+(defmacro with-parameters (env keys &rest body)
+  `(destructuring-bind (&key ,@keys)
+       (getf ,env :route.parameters)
+     ,@body))
 
 (defun index (env)
   '(200
     (:content-type "text/plain")
     ("Hello world!")))
 
+(defun user-get (env)
+  (with-parameters env (user)
+    `(200
+      (:content-type "text/plain")
+      (,(format nil "Hey hey you are ~a~%" user)))))
+
 (defroutes app
  (GET "/" #'index)
  (GET "/user" #'user-list)
- (GET "/user/:user" #'uset-get)
+ (GET "/user/:user" #'user-get)
  (GET "/server" #'server-list)
  (GET "/server/:server" #'server-get)
  (GET "/channel" #'channel-list)
