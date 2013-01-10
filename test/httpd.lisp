12b60d07a30f3ffbf26052ed92cf4d743d98b9bb
Julien Danjou <julien@danjou.info>
2013-01-10 23:40:11 +0100

    Make tests faster
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- test/httpd.lisp
+++ test/httpd.lisp
@@ -11,32 +11,35 @@
 (defun decode-json-body (body)
   (decode-json-from-string (flex:octets-to-string body)))
 
-(plan 12)
+(plan 18)
+
+(defvar *tests* nil
+  "Test list.")
 
 (defmacro do-test (url comment &rest body)
-  `(test-app
-    #'app
-    (lambda ()
-      (multiple-value-bind (body status headers)
-          (http-request ,url)
-        ,@body))
-    ,comment))
+  `(push ',(list url comment body) *tests*))
 
 (do-test "http://localhost:4242/user"
   "Testing user listing"
-  (is status 200)
+  (is status 200 "Status code 200")
   (is (decode-json-body body) '(((:name . "jd"))))
   (is (cdr (assoc :content-type headers)) "application/json"))
 
 (do-test "http://localhost:4242/user/jd"
   "Testing user retrieval"
-  (is status 200)
+  (is status 200 "Status code 200")
   (is (decode-json-body body) '((:name . "jd")))
+  (is (cdr (assoc :content-type headers)) "application/json"))
+
+(do-test "http://localhost:4242/user/foobar"
+  "Testing non-existent user retrieval"
+  (is status 404 "Status code 404")
+  (is (decode-json-body body) '((:status . "Not Found") (:message . "No such user")))
   (is (cdr (assoc :content-type headers)) "application/json"))
 
 (do-test "http://localhost:4242/server"
   "Testing server listing"
-  (is status 200)
+  (is status 200 "Status code 200")
   (is (decode-json-body body) '(((:name . "Naquadah")
                                  (:address . "irc.naquadah.org")
                                  (:port . 6667)
@@ -45,9 +48,28 @@
 
 (do-test "http://localhost:4242/server/Naquadah"
   "Testing server retrieval"
-  (is status 200)
+  (is status 200 "Status code 200")
   (is (decode-json-body body) '((:name . "Naquadah")
                                 (:address . "irc.naquadah.org")
                                 (:port . 6667)
                                 (:ssl . t)))
   (is (cdr (assoc :content-type headers)) "application/json"))
+
+(do-test "http://localhost:4242/server/foobar"
+  "Testing non-existent server retrieval"
+  (is status 404 "Status code 404")
+  (is (decode-json-body body) '((:status . "Not Found") (:message . "No such server")))
+  (is (cdr (assoc :content-type headers)) "application/json"))
+
+
+;; Really run the tests now
+(test-app
+ #'app
+ (lambda ()
+   (loop for test in *tests*
+         do (multiple-value-bind (body status headers)
+                (http-request (first test))
+              (declare (special body status headers))
+              (diag (second test))
+              (dolist (test-body (third test))
+                (eval test-body))))))
