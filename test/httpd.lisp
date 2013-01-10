07296a2dd36e776580549c848cd1eae39f206d4d
Julien Danjou <julien@danjou.info>
2013-01-10 17:51:12 +0100

    Factorize test and add user retrieval
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- test/httpd.lisp
+++ test/httpd.lisp
@@ -11,14 +11,25 @@
 (defun decode-json-body (body)
   (decode-json-from-string (flex:octets-to-string body)))
 
-(plan 3)
+(plan 6)
 
-(test-app
- #'app
- (lambda ()
-   (multiple-value-bind (body status headers)
-       (http-request "http://localhost:4242/user")
-     (is status 200)
-     (is (decode-json-body body) '(((:name . "jd"))))
-     (is (cdr (assoc :content-type headers)) "application/json")))
- "Testing Kawoosh httpd")
+(defmacro do-test (url comment &rest body)
+  `(test-app
+    #'app
+    (lambda ()
+      (multiple-value-bind (body status headers)
+          (http-request ,url)
+        ,@body))
+    ,comment))
+
+(do-test "http://localhost:4242/user"
+  "Testing user listing"
+  (is status 200)
+  (is (decode-json-body body) '(((:name . "jd"))))
+  (is (cdr (assoc :content-type headers)) "application/json"))
+
+(do-test "http://localhost:4242/user/jd"
+  "Testing user retrieval"
+  (is status 200)
+  (is (decode-json-body body) '((:name . "jd")))
+  (is (cdr (assoc :content-type headers)) "application/json"))
