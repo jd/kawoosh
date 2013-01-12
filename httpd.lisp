24452900e49536cfa458bc65df9f23c323bab154
Julien Danjou <julien@danjou.info>
2013-01-12 13:03:05 +0100

    Simplify encode-json override
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- httpd.lisp
+++ httpd.lisp
@@ -10,16 +10,15 @@
 
 (in-package :kawoosh.httpd)
 
-
-(defmethod json:encode-json ((o dao-object)
-                             &optional (stream json:*json-output*))
+(defmethod encode-json ((o kawoosh.dao:dao-object)
+                        &optional (stream json:*json-output*))
   "Write the JSON representation (Object) of the postmodern DAO CLOS object
 O to STREAM (or to *JSON-OUTPUT*)."
-  (json:with-object (stream)
+  (with-object (stream)
     (json::map-slots (lambda (key value)
-                       (as-object-member (key stream)
-                         (json:encode-json (if (eq value :null) nil value) stream)))
-                     o)))
+                 (as-object-member (key stream)
+                   (encode-json (if (eq value :null) nil value) stream)))
+               o)))
 
 (defmacro with-parameters (env keys &rest body)
   `(destructuring-bind (&key ,@keys)
